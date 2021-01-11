ec6ab9f443ac2204bcdc81cbcc55ad07ae1f61d0
najmdin <aitbelarbinajmeddine@gmail.com>
2021-01-11 13:48:48 +0100

    premier commit

--- resources/views/studentslist.blade.php
+++ resources/views/studentslist.blade.php
@@ -1,19 +1,19 @@
 
 <div class="card mb-3">
-    <img src="https://cdn.pixabay.com/photo/2015/05/19/14/55/educational-773651_960_720.jpg" class="card-img-top" alt="...">
+    <img src="https://www.letudiant.fr/static/uploads/mediatheque/EDU_EDU/0/0/189700-universite-paris-dauphine-bibliotheque-m-a-nourry-fevrier-2014-580x310.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-        <h5 class="card-title">List of students</h5>
-        <p class="card-text">You can find here all the informatins about students in the system</p>
+        <h5 class="card-title">Liste des étudiants</h5>
+        <p class="card-text">Vous pouvez trouvez toutes les informations des étudiants dans le système</p>
 
         <table class="table">
             <thead class="thead-light">
             <tr>
                 <th scope="col">CNE</th>
-                <th scope="col">First name</th>
-                <th scope="col">Second Name</th>
+                <th scope="col">Prénom</th>
+                <th scope="col">Nom</th>
                 <th scope="col">Age</th>
-                <th scope="col">Speciality</th>
-                <th scope="col">Operations</th>
+                <th scope="col">Spécialité</th>
+                <th scope="col">Opérations</th>
 
             </tr>
             </thead>
@@ -27,7 +27,7 @@
                     <td>{{ $student->speciality }}</td>
                     <td>
 
-                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Modifier</a>
 
                     </td>
 
